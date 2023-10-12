db0625f47c26cb1ca9704522051e5edcfbc6e20d
supercutefish <137264552+supercutefish@users.noreply.github.com>
2023-10-12 20:31:13 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,7 +67,7 @@ VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
-TW_THEME := watch_hdpi
+TW_THEME := watch_mdpi
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
